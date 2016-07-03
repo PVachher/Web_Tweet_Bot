1838043ee4b45d169ba0f446b8e4927de5d3990b
Prateek <vachherprateek@gmail.com>
2016-07-03 16:23:34 +0530

    Sample MYSQL File

---
+++ Sample MYSQL Python.py
@@ -0,0 +1,19 @@
+
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("vachher.ddns.net","remote","Welcome@1234","TestDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+
+print "Database version : %s " % data
+
+# disconnect from server
+db.close()
