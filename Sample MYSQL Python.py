2054d980a40942c62b969cfcd35c8015ce6d33ad
Prateek Vachher <vachherprateek@gmail.com>
2016-07-03 22:47:25 +0530

    Testing MySQL Connection

--- Sample MYSQL Python.py
+++ Sample MYSQL Python.py
@@ -1,4 +1,3 @@
-
 import MySQLdb
 
 # Open database connection
@@ -8,7 +7,7 @@ db = MySQLdb.connect("vachher.ddns.net","remote","Welcome@1234","TestDB" )
 cursor = db.cursor()
 
 # execute SQL query using execute() method.
-cursor.execute("SELECT VERSION()")
+cursor.execute("SELECT * FROM `counter`;")
 
 # Fetch a single row using fetchone() method.
 data = cursor.fetchone()
